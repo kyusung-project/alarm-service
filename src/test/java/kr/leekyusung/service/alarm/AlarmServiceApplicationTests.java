b2594604b6d54443d7e5f50085d22e88faa5bdad
LeeKyuSung <ks5050577@gmail.com>
2023-09-10 20:17:56 +0900

    Initial commit

---
+++ src/test/java/kr/leekyusung/service/alarm/AlarmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.leekyusung.service.alarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
