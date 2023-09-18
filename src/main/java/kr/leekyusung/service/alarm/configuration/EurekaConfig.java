fd3a5d84ea3eb01a21fedbe1f14b80ed0249af4f
LeeKyuSung <ks5050577@gmail.com>
2023-09-19 01:12:08 +0900

    플젝 기본 세팅
    Create Dockerfile

---
+++ src/main/java/kr/leekyusung/service/alarm/configuration/EurekaConfig.java
@@ -0,0 +1,27 @@
+package kr.leekyusung.service.alarm.configuration;
+
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Configuration
+public class EurekaConfig {
+    @Bean
+    public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+        EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+        String ip = null;
+        try {
+            ip = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        config.setIpAddress(ip);
+        config.setPreferIpAddress(true);
+        config.setNonSecurePort(8080);
+        return config;
+    }
+}
