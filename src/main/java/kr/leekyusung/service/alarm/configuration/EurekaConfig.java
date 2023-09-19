0df34ac18d29e6097797718027b203e0832a42fc
LeeKyuSung <ks5050577@gmail.com>
2023-09-20 00:56:35 +0900

    aws 최종 세팅

--- src/main/java/kr/leekyusung/service/alarm/configuration/EurekaConfig.java
+++ src/main/java/kr/leekyusung/service/alarm/configuration/EurekaConfig.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -11,6 +12,7 @@ import java.net.UnknownHostException;
 @Configuration
 public class EurekaConfig {
     @Bean
+    @Profile("prod")
     public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
         EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
         String ip = null;
